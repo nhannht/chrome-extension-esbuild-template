316674c7e2a12facd5355a4e04e981d368a9c54c
nhannht <nhanclassroom@gmail.com>
2023-01-25 01:10:19 +0700

    various fix

--- build.mjs
+++ build.mjs
@@ -10,10 +10,7 @@ const defaultScript = process.argv.includes("--default");
 const content = process.argv.includes("--content");
 const background = process.argv.includes("--background");
 const setting = process.argv.includes("--settingComponent");
-/**
- * ESBuild Params
- * @link https://esbuild.github.io/api/#build-api
- */
+
 let entries = [];
 if (defaultScript) {
     entries.push("src/default.jsx");
