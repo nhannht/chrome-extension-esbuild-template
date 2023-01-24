beec9af269c0c6d37676efe70a63256910098d88
nhannht <nhanclassroom@gmail.com>
2023-01-24 15:35:05 +0700

    various fix

--- src/setting.jsx
+++ src/setting.jsx
@@ -1,10 +1,2 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import SettingIndex from "./setting/SettingIndex";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root?.render(
-    <React.StrictMode>
-        <SettingIndex/>
-    </React.StrictMode>
-);
+import {DevSupport} from "@react-buddy/ide-toolbox";
+import {ComponentPreviews, useInitial} from "./dev";
