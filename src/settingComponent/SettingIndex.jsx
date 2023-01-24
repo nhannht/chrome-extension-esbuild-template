a3bd469e6e3e5f0779d153ef0294dc2a73233f16
nhannht <nhanclassroom@gmail.com>
2023-01-24 18:16:31 +0700

    various fix

--- src/settingComponent/SettingIndex.jsx
+++ src/settingComponent/SettingIndex.jsx
@@ -4,13 +4,12 @@ export default function SettingIndex() {
     const [number, setNumber] = React.useState(0)
 
     return (
-        <div className={'container w-full mx-auto flex flex-col'}>
-            <button
-                className={'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'}
-                onClick={() => setNumber(number + 1)}>Click me
+        <div
+            className={'container  mx-auto flex flex-col place-content-center place-items-center place-self-center'}>
+            <button type="button"
+                    className={'bg-gray-500 hover:bg-black text-white font-bold py-2 px-4 rounded'}
+                    onClick={() => setNumber(number + 1)}>Click me
             </button>
-            <div className={'text-2xl font-bold'}>This is setting view header</div>
-            <div className={'font-bold'}>This is setting view header</div>
             <div className={'text-red-600'}>{number}</div>
         </div>
     )
